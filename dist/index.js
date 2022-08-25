186ccf1826705cbd09abdd63a4b0c963c0645f16
dedenbangkit <mail@dedenbangkit.com>
2022-08-25 12:22:13 +0700

    [#1] Cache Tryout

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var React = _interopDefault(require('react'));
+
+var styles = {"test":"arfe-test"};
+
+var WebformEditor = function WebformEditor() {
+  return /*#__PURE__*/React.createElement("div", {
+    className: styles.test
+  }, "Webform Component");
+};
+
+module.exports = WebformEditor;
+//# sourceMappingURL=index.js.map
---
+++ dist/index.modern.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+var styles = {"test":"arfe-test"};
+
+var WebformEditor = function WebformEditor() {
+  return /*#__PURE__*/React.createElement("div", {
+    className: styles.test
+  }, "Webform Component");
+};
+
+export default WebformEditor;
+//# sourceMappingURL=index.modern.js.map
