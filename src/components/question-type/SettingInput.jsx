9e54b387814205b30f6ae677e7ca8d2944ceb2b1
wayangalihpratama <wgprtm@gmail.com>
2022-09-02 14:21:37 +0800

    [#3] Handle on change on SettingInput component

--- src/components/question-type/SettingInput.jsx
+++ src/components/question-type/SettingInput.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
 import { Form, Checkbox, Space } from 'antd';
 import styles from '../../styles.module.css';
-import { UIStore } from '../../lib/store';
+import { UIStore, questionGroupFn } from '../../lib/store';
 
-const SettingInput = ({ id }) => {
+const SettingInput = ({ id, questionGroupId }) => {
   const namePreffix = `question-${id}`;
   const UIText = UIStore.useState((s) => s.UIText);
+
+  const updateState = (name, value) => {
+    questionGroupFn.store.update((s) => {
+      s.questionGroups = s.questionGroups.map((qg) => {
+        if (qg.id === questionGroupId) {
+          const questions = qg.questions.map((q) => {
+            if (q.id === id) {
+              return {
+                ...q,
+                [name]: value,
+              };
+            }
+            return q;
+          });
+          return {
+            ...qg,
+            questions: questions,
+          };
+        }
+        return qg;
+      });
+    });
+  };
+
+  const handleChangeDoubleEntry = (e) => {
+    updateState('requiredDoubleEntry', e?.target?.checked);
+  };
+
+  const handleChangeHiddenString = (e) => {
+    updateState('hiddenString', e?.target?.checked);
+  };
 
   return (
     <div>
@@ -17,13 +48,19 @@ const SettingInput = ({ id }) => {
           initialValue={false}
           name={`${namePreffix}-require_double_entry`}
         >
-          <Checkbox> {UIText.inputQuestionRequireDoubleEntryCheckbox}</Checkbox>
+          <Checkbox onChange={handleChangeDoubleEntry}>
+            {' '}
+            {UIText.inputQuestionRequireDoubleEntryCheckbox}
+          </Checkbox>
         </Form.Item>
         <Form.Item
           initialValue={false}
           name={`${namePreffix}-hidden_string`}
         >
-          <Checkbox> {UIText.inputQuestionHiddenStringCheckbox}</Checkbox>
+          <Checkbox onChange={handleChangeHiddenString}>
+            {' '}
+            {UIText.inputQuestionHiddenStringCheckbox}
+          </Checkbox>
         </Form.Item>
       </Space>
     </div>
