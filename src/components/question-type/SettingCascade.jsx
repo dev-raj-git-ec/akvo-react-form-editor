54b09923d2c653efa91c48688060941779b920cc
wayangalihpratama <wgprtm@gmail.com>
2022-09-05 17:43:11 +0800

    [#3] Handle onchange event on SettingCascade

--- src/components/question-type/SettingCascade.jsx
+++ src/components/question-type/SettingCascade.jsx
@@ -1,11 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Checkbox, Row, Col, Input, InputNumber } from 'antd';
 import styles from '../../styles.module.css';
-import { UIStore } from '../../lib/store';
+import { UIStore, questionGroupFn } from '../../lib/store';
 
-const SettingCascade = ({ id, api }) => {
+const defaultApiValue = {
+  endpoint: null,
+  initial: 0,
+  list: false,
+};
+
+const SettingCascade = ({ id, questionGroupId, api }) => {
   const namePreffix = `question-${id}`;
   const UIText = UIStore.useState((s) => s.UIText);
+  const [apiValue, setApiValue] = useState(defaultApiValue);
+
+  useEffect(() => {
+    // update global state
+    if (apiValue?.endpoint) {
+      questionGroupFn.store.update((s) => {
+        s.questionGroups = s.questionGroups.map((qg) => {
+          if (qg.id === questionGroupId) {
+            const questions = qg.questions.map((q) => {
+              if (q.id === id) {
+                return {
+                  ...q,
+                  api: apiValue,
+                };
+              }
+              return q;
+            });
+            return {
+              ...qg,
+              questions: questions,
+            };
+          }
+          return qg;
+        });
+      });
+    }
+  }, [apiValue, id, questionGroupId]);
+
+  const updateLocalState = (name, value) => {
+    const updatedApiValue = {
+      ...apiValue,
+      [name]: value,
+    };
+    setApiValue(updatedApiValue);
+  };
+
+  const handleChangeEndpoint = (e) => {
+    updateLocalState('endpoint', e?.target?.value);
+  };
+
+  const handleChangeInitial = (e) => {
+    updateLocalState('initial', e);
+  };
+
+  const handleChangeList = (e) => {
+    updateLocalState('list', e?.target?.checked);
+  };
 
   return (
     <div>
@@ -14,10 +67,11 @@ const SettingCascade = ({ id, api }) => {
       </p>
       <Form.Item
         label={UIText.inputQuestionEndpointLabel}
-        initialValue={api?.endpoint || null}
+        initialValue={api?.endpoint || defaultApiValue.endpoint}
         name={`${namePreffix}-api-endpoint`}
+        rules={[{ type: 'url', message: 'Not valid URL' }]}
       >
-        <Input />
+        <Input onChange={handleChangeEndpoint} />
       </Form.Item>
       <Row
         align="bottom"
@@ -26,22 +80,26 @@ const SettingCascade = ({ id, api }) => {
         <Col span={4}>
           <Form.Item
             label={UIText.inputQuestionInitialValueLabel}
-            initialValue={api?.initial || null}
+            initialValue={api?.initial || defaultApiValue.initial}
             name={`${namePreffix}-api-initial`}
           >
             <InputNumber
               style={{ width: '100%' }}
               controls={false}
               keyboard={false}
+              onChange={handleChangeInitial}
             />
           </Form.Item>
         </Col>
         <Col>
           <Form.Item
-            initialValue={api?.list || null}
+            initialValue={api?.list || defaultApiValue.list}
             name={`${namePreffix}-api-list`}
           >
-            <Checkbox> {UIText.inputQuestionListCheckbox}</Checkbox>
+            <Checkbox onChange={handleChangeList}>
+              {' '}
+              {UIText.inputQuestionListCheckbox}
+            </Checkbox>
           </Form.Item>
         </Col>
       </Row>
