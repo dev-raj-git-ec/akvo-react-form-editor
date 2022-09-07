b0bbfebf131272897c19ac7459f4937368a0c5d8
wayangalihpratama <wgprtm@gmail.com>
2022-09-07 17:13:15 +0800

    [#3] Update SettingCascade, support use spesific object name input

--- src/components/question-type/SettingCascade.jsx
+++ src/components/question-type/SettingCascade.jsx
@@ -1,65 +1,57 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Form, Checkbox, Row, Col, Input, InputNumber } from 'antd';
 import styles from '../../styles.module.css';
 import { UIStore, questionGroupFn } from '../../lib/store';
 
-const defaultApiValue = {
-  endpoint: null,
-  initial: 0,
-  list: false,
-};
-
-const SettingCascade = ({ id, questionGroupId, api = defaultApiValue }) => {
+const SettingCascade = ({
+  id,
+  questionGroupId,
+  api = {
+    endpoint: null,
+    initial: 0,
+    list: false,
+  },
+}) => {
   const namePreffix = `question-${id}`;
   const UIText = UIStore.useState((s) => s.UIText);
-  const [apiValue, setApiValue] = useState(
-    !Object.keys(api).length ? defaultApiValue : api
-  );
 
-  useEffect(() => {
-    // update global state
-    if (apiValue?.endpoint) {
-      questionGroupFn.store.update((s) => {
-        s.questionGroups = s.questionGroups.map((qg) => {
-          if (qg.id === questionGroupId) {
-            const questions = qg.questions.map((q) => {
-              if (q.id === id) {
-                return {
-                  ...q,
-                  api: apiValue,
-                };
-              }
-              return q;
-            });
-            return {
-              ...qg,
-              questions: questions,
-            };
-          }
-          return qg;
-        });
+  const updateGlobalState = (name, value) => {
+    questionGroupFn.store.update((s) => {
+      s.questionGroups = s.questionGroups.map((qg) => {
+        if (qg.id === questionGroupId) {
+          const questions = qg.questions.map((q) => {
+            if (q.id === id) {
+              return {
+                ...q,
+                api: {
+                  ...q?.api,
+                  [name]: value,
+                },
+              };
+            }
+            return q;
+          });
+          return {
+            ...qg,
+            questions: questions,
+          };
+        }
+        return qg;
       });
-    }
-  }, [apiValue, id, questionGroupId]);
-
-  const updateLocalState = (name, value) => {
-    const updatedApiValue = {
-      ...apiValue,
-      [name]: value,
-    };
-    setApiValue(updatedApiValue);
+    });
   };
 
   const handleChangeEndpoint = (e) => {
-    updateLocalState('endpoint', e?.target?.value);
+    updateGlobalState('endpoint', e?.target?.value);
+    updateGlobalState('initial', 0);
   };
 
   const handleChangeInitial = (e) => {
-    updateLocalState('initial', e);
+    updateGlobalState('initial', e);
   };
 
-  const handleChangeList = (e) => {
-    updateLocalState('list', e?.target?.checked);
+  const handleChangeList = (value) => {
+    updateGlobalState('list', value);
   };
 
   return (
@@ -69,7 +61,7 @@ const SettingCascade = ({ id, questionGroupId, api = defaultApiValue }) => {
       </p>
       <Form.Item
         label={UIText.inputQuestionEndpointLabel}
-        initialValue={api?.endpoint || defaultApiValue.endpoint}
+        initialValue={api?.endpoint}
         name={`${namePreffix}-api-endpoint`}
         rules={[
           { type: 'url', message: UIText.inputQuestionEndpointValidationText },
@@ -84,7 +76,7 @@ const SettingCascade = ({ id, questionGroupId, api = defaultApiValue }) => {
         <Col span={4}>
           <Form.Item
             label={UIText.inputQuestionInitialValueLabel}
-            initialValue={api?.initial || defaultApiValue.initial}
+            initialValue={api?.initial}
             name={`${namePreffix}-api-initial`}
           >
             <InputNumber
@@ -96,16 +88,29 @@ const SettingCascade = ({ id, questionGroupId, api = defaultApiValue }) => {
           </Form.Item>
         </Col>
         <Col>
-          <Form.Item name={`${namePreffix}-api-list`}>
+          <Form.Item name={`${namePreffix}-api-list-checkbox`}>
             <Checkbox
-              onChange={handleChangeList}
-              checked={api?.list || defaultApiValue.list}
+              onChange={(e) => handleChangeList(e?.target?.checked)}
+              checked={api?.list ? true : false}
             >
               {' '}
               {UIText.inputQuestionListCheckbox}
             </Checkbox>
           </Form.Item>
         </Col>
+        {api?.list && (
+          <Col span={8}>
+            <Form.Item
+              label={UIText.inputQuestionListLabel}
+              initialValue={
+                api?.list ? (api.list !== true ? api.list : null) : null
+              }
+              name={`${namePreffix}-api-list`}
+            >
+              <Input onChange={(e) => handleChangeList(e?.target?.value)} />
+            </Form.Item>
+          </Col>
+        )}
       </Row>
     </div>
   );
