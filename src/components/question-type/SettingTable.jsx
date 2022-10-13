f31f1dc95d413e2e55d4d057d80224b9a621f8ab
wayangalihpratama <wgprtm@gmail.com>
2022-10-13 20:47:58 +0800

    [#19] Add text column type & initial setup for option column type

--- src/components/question-type/SettingTable.jsx
+++ src/components/question-type/SettingTable.jsx
@@ -10,6 +10,7 @@ const columnType = {
   input: 'input',
   number: 'number',
   option: 'option',
+  text: 'text',
 };
 
 const defaultColumns = ({ init = false }) => {
@@ -24,17 +25,42 @@ const defaultColumns = ({ init = false }) => {
   return { ...column, id: generateId() };
 };
 
+const defaultColumnOptions = ({ init = false, order = 0 }) => {
+  const option = {
+    name: 'New Option',
+    order: 1,
+  };
+  if (init) {
+    return [{ ...option, id: generateId() }];
+  }
+  return { ...option, id: generateId(), order: order };
+};
+
 const SettingTable = ({ id, questionGroupId, columns: initialColumns }) => {
   const namePreffix = `question-${id}`;
   const UIText = UIStore.useState((s) => s.UIText);
   const [columns, setColumns] = useState(
     initialColumns?.length ? initialColumns : defaultColumns({ init: true })
   );
+  const [options, setOptions] = useState(defaultColumnOptions({ init: true }));
 
   const columnTypeOptions = Object.keys(columnType).map((key) => ({
     label: columnType[key],
     value: key,
   }));
+
+  const handleChangeColumnType = (columnId, value) => {
+    const updatedColumn = columns.map((cl) => {
+      if (cl.id === columnId) {
+        return {
+          ...cl,
+          type: value,
+        };
+      }
+      return cl;
+    });
+    setColumns(updatedColumn);
+  };
 
   return (
     <div className={styles['more-question-setting-text']}>
@@ -45,7 +71,7 @@ const SettingTable = ({ id, questionGroupId, columns: initialColumns }) => {
             key={`column-${id}-${cli}-${cl.id}`}
             title={`Column - ${cli + 1}`}
           >
-            <Row gutter={[24, 24]}>
+            <Row gutter={[24, 20]}>
               <Col span={12}>
                 <Form.Item
                   name={`${namePreffix}-${cl.name}`}
@@ -60,6 +86,7 @@ const SettingTable = ({ id, questionGroupId, columns: initialColumns }) => {
                   name={`${namePreffix}-${cl.type}`}
                   className={styles['form-item-no-bottom-margin']}
                   label={UIText.inputColumnTypeLabel}
+                  initialValue={cl.type}
                 >
                   <Select
                     showSearch
@@ -68,8 +95,31 @@ const SettingTable = ({ id, questionGroupId, columns: initialColumns }) => {
                     getPopupContainer={(triggerNode) =>
                       triggerNode.parentElement
                     }
+                    onChange={(val) => handleChangeColumnType(cl.id, val)}
                   />
                 </Form.Item>
+              </Col>
+              <Col span={24}>
+                {cl.type === columnType.option && (
+                  <div>
+                    <p>Set an Option</p>
+                    {options.map((op, opi) => {
+                      return (
+                        <Row
+                          key={`option-${id}-${opi}-${op.id}`}
+                          gutter={[24, 24]}
+                        >
+                          <Col span={12}>
+                            <Form.Item initialValue={op.name}>
+                              <Input allowClear />
+                            </Form.Item>
+                          </Col>
+                          <Col>+</Col>
+                        </Row>
+                      );
+                    })}
+                  </div>
+                )}
               </Col>
             </Row>
           </Card>
