408e86ce7399f89c2d7251c00931fab541b5e617
wayangalihpratama <wgprtm@gmail.com>
2024-05-29 19:16:19 +0800

    [#61] Enable validate fnString onBlur

--- src/components/question-type/SettingAutofield.jsx
+++ src/components/question-type/SettingAutofield.jsx
@@ -1,7 +1,12 @@
 import React, { useState, useMemo } from 'react';
-import { Form, Checkbox, Space, Input, AutoComplete } from 'antd';
+import { Form, Checkbox, Space, Input, AutoComplete, Typography } from 'antd';
 import styles from '../../styles.module.css';
-import { UIStore, questionGroupFn, questionType } from '../../lib/store';
+import {
+  UIStore,
+  questionGroupFn,
+  questionType,
+  ErrorStore,
+} from '../../lib/store';
 import isEmpty from 'lodash/isEmpty';
 
 const fnStringExample =
@@ -18,6 +23,8 @@ const allowedQuestionTypes = [
   questionType.autofield,
 ];
 
+const { Text } = Typography;
+
 const SettingAutofield = ({
   id,
   questionGroupId,
@@ -29,13 +36,31 @@ const SettingAutofield = ({
     (s) => s.questionGroups
   );
   const [search, setSearch] = useState(null);
+  const questionErrors = ErrorStore.useState((s) => s.questionErrors);
+
+  const currentAutofieldFnStringError = useMemo(() => {
+    const findError = questionErrors.find(
+      (e) => e.id === id && e.field === 'autofield_fnString'
+    );
+    if (findError) {
+      return findError;
+    }
+    return false;
+  }, [id, questionErrors]);
+
+  const allAllowedQuestions = questionGroups
+    .flatMap((qg) => qg.questions)
+    .filter((q) => allowedQuestionTypes.includes(q.type))
+    .map((q) => ({
+      id: q.id,
+      name: q.name,
+      type: q.type,
+    }));
 
   const questionNames = useMemo(() => {
     let preffix = '';
     const regex = new RegExp(search, 'gi');
-    let res = questionGroups
-      .flatMap((qg) => qg.questions)
-      .filter((q) => q.id !== id && allowedQuestionTypes.includes(q.type));
+    let res = allAllowedQuestions.filter((q) => q.id !== id);
     if (search) {
       res = res.filter((q) => q.name.match(regex)?.length);
     }
@@ -46,7 +71,64 @@ const SettingAutofield = ({
       label: `${preffix}#${q.name}#`,
       value: `${preffix}#${q.name}#`,
     }));
-  }, [questionGroups, id, search, fn.fnString]);
+  }, [allAllowedQuestions, id, search, fn.fnString]);
+
+  const sampleValues = useMemo(() => {
+    return allAllowedQuestions.reduce((acc, q) => {
+      const key = `#${q.name}#`;
+      let sampleValue = 0;
+      if (
+        [
+          questionType.input,
+          questionType.text,
+          questionType.autofield,
+        ].includes(q.type)
+      ) {
+        sampleValue = 'lorem';
+      } else if (q.type === questionType.option) {
+        sampleValue = 'option_lorem';
+      } else if (q.type === questionType.multiple_option) {
+        sampleValue = 'option_lorem | option_ipsum';
+      }
+      return {
+        ...acc,
+        [key]: sampleValue,
+      };
+    }, {});
+  }, [allAllowedQuestions]);
+
+  const validateAndExecute = (fnStringValue) => {
+    // Replace placeholders with sample values
+    let processedStr = fnStringValue;
+    Object.keys(sampleValues).forEach((key) => {
+      const placeholder = new RegExp(key, 'g');
+      const value = sampleValues[key];
+      processedStr = processedStr.replace(placeholder, JSON.stringify(value));
+    });
+
+    try {
+      new Function(`return ${processedStr}`)();
+      ErrorStore.update((s) => {
+        // remove from error list
+        s.questionErrors = s.questionErrors.filter(
+          (e) => e.id !== id && e.field !== 'autofield_fnString'
+        );
+      });
+    } catch (error) {
+      ErrorStore.update((s) => {
+        s.questionErrors = [
+          ...s.questionErrors,
+          {
+            id: id,
+            field: 'autofield_fnString',
+            message: `Error evaluating function string${
+              error?.message ? `: ${error.message}` : ''
+            }`,
+          },
+        ];
+      });
+    }
+  };
 
   const updateState = (name, value) => {
     questionGroupFn.store.update((s) => {
@@ -87,6 +169,7 @@ const SettingAutofield = ({
   const handleBlurFnString = (e) => {
     setSearch(null);
     const val = e?.target?.value;
+    validateAndExecute(val);
     updateState('fn', { ...fn, fnString: val });
   };
 
@@ -154,6 +237,13 @@ const SettingAutofield = ({
           />
         </AutoComplete>
       </Form.Item>
+      {currentAutofieldFnStringError?.id ? (
+        <div className={styles['field-error-wrapper']}>
+          <Text type="danger">{currentAutofieldFnStringError.message}</Text>
+        </div>
+      ) : (
+        ''
+      )}
       <Form.Item
         label={UIText.inputQuestionAutofieldFnColor}
         name={`${namePreffix}-autofield_fnColor`}
